88cbd5197a4dc1028054c5e8249329fc2a9e710e
Lauren Tanea <taneajoshi@gmail.com>
2022-07-03 21:52:13 +0530

    improving the application by adding lifecyle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-        return <li key={igKey}><span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-    })
+class OrderSummary  extends Component {
+    UNSAFE_componentWillUpdate() {
+        console.log('[OrderSummary] Will update');
+    }
+        render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return <li key={igKey}><span style={{textTransform: "capitalize"}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+        })
+        return(
+                <Aux>
+                    <h3>Your Order</h3>
+                    <p>A delecious burger with following ingredients: </p>
+                    <ul>
+                        {ingredientSummary}
+                    </ul>
+                    <p>Continue to checkout</p>
+                    <p><strong>Price:</strong> ${this.props.totalPrice.toFixed(2)}</p>
+                    <Button btnType="danger" clicked={this.props.purchaseCancled}>CANCEL</Button>
+                    <Button btnType="success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+                </Aux>
+        )
+    }
+}
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delecious burger with following ingredients: </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to checkout</p>
-            <p><strong>Price:</strong> ${props.totalPrice.toFixed(2)}</p>
-            <Button btnType="danger" clicked={props.purchaseCancled}>CANCEL</Button>
-            <Button btnType="success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    );
-};
-
-export default OrderSummary
+export default OrderSummary;
--- src/components/UI/CustomModal/CustomModal.js
+++ src/components/UI/CustomModal/CustomModal.js
@@ -1,23 +1,33 @@
 import './CustomModal.scss';
 import Aux from '../../../hoc/Aux';
-import React from 'react'
+import React, { Component } from 'react'
 import Backdrop from '../Backdrop/Backdrop';
 
-const CustomModal = (props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div 
-      className='custom-modal'
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-      >
-        {props.children}
-    </div>
-    </Aux>
-  )
+class CustomModal extends Component{
+  shouldComponentUpdate(nextProps, nextState) {
+   return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate () {
+    console.log('Modal Will update');
+  }
+  
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div 
+        className='custom-modal'
+        style={{
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? '1' : '0'
+        }}
+        >
+          {this.props.children}
+      </div>
+      </Aux>
+    )
+  }
 }
 
-export default CustomModal
+export default CustomModal;
